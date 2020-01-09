8e6451312813e2e3ddf8d7462fd30b6fd749f694
Madar Gergő <madargergo95@gmail.com>
2020-01-09 11:37:58 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
